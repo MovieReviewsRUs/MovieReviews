35e6b093f06369fa095a051b518c4de39efcf2c1
Helen Thi <ht5658@gmail.com>
2015-03-05 17:13:33 -0500

    fixed seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@ Movie.destroy_all
 
 movies = Movie.create([
   {title: 'The Lego Move', total_gross: 257756197, release_date: DateTime.new(2014,2,07,9), mpaa_rating: 'pg', description: 'rate this movie!'},
-  {title: '22 Jump Street', total_gross: 191616238, release_date: 2010, mpaa_rating: DateTime.new(2014,6,13,9), description: 'rate this movie!'},
-  {title: 'GoneGirl', total_gross: 167735396, release_date: DateTime.new(2014,10,03,9), mpaa_rating: , description: 'rate this movie!'},
+  {title: '22 Jump Street', total_gross: 191616238, release_date: DateTime.new(2014,6,13,9), mpaa_rating: 'r', description: 'rate this movie!'},
+  {title: 'GoneGirl', total_gross: 167735396, release_date: DateTime.new(2014,10,03,9), mpaa_rating: 'r', description: 'rate this movie!'}
   ])
+
