152f8af7a00613e254008d2f994da8190eaad906
pat <patrick.h.needham@gmail.com>
2015-03-05 16:34:12 -0500

    seeds, updated movie table column to datetime

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 Movie.destroy_all
 
 movies = Movie.create([
-  {title: 'Forrest Gump', total_gross: 1000000, release_date: 2010, mpaa_rating: , description: },
-  {title: 'Forrest Gump', total_gross: 1000000, release_date: 2010, mpaa_rating: , description: },
-  {title: 'Forrest Gump', total_gross: 1000000, release_date: 2010, mpaa_rating: , description: },
+  {title: 'The Lego Move', total_gross: 257756197, release_date: DateTime.new(2014,2,07,9), mpaa_rating: 'pg', description: 'rate this movie!'},
+  {title: '22 Jump Street', total_gross: 191616238, release_date: 2010, mpaa_rating: DateTime.new(2014,6,13,9), description: 'rate this movie!'},
+  {title: 'GoneGirl', total_gross: 167735396, release_date: DateTime.new(2014,10,03,9), mpaa_rating: , description: 'rate this movie!'},
   ])
