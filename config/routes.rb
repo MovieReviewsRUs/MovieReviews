fe5652a34d0ac6c441a66789b29013b46c1df8a9
Helen Thi <ht5658@gmail.com>
2015-03-05 17:31:16 -0500

    added routes for reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :movies, only: [:index, :create, :show, :update, :destroy]
+  resources :movies, only: [:index, :create, :show, :update, :destroy] do
+    resources :reviews, only: [:index, :create]
+  end
 end
