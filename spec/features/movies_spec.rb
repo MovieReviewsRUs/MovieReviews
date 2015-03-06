d1ddcaa6e05ba790783874dba34fe9507b866073
pat <patrick.h.needham@gmail.com>
2015-03-06 14:42:44 -0500

    spec updates, require capybara in rails_helper

--- spec/features/movies_spec.rb
+++ spec/features/movies_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 describe 'Movie Requests' do
   before(:all) do
     Movie.destroy_all
-    movies = Movie.create([
+    @movies = Movie.create([
       {title: 'The Lego Move', total_gross: 257756197, release_date: DateTime.new(2014,2,07,9), mpaa_rating: 'pg', description: 'rate this movie!'},
       {title: '22 Jump Street', total_gross: 191616238, release_date: DateTime.new(2014,6,13,9), mpaa_rating: 'r', description: 'rate this movie!'},
       {title: 'Gone Girl', total_gross: 167735396, release_date: DateTime.new(2014,10,03,9), mpaa_rating: 'r', description: 'rate this movie!'},
