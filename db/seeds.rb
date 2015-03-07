e615b6f824d0134ae4ec8e5dba6538fc61825471
Helen Thi <ht5658@gmail.com>
2015-03-06 21:54:37 -0500

    added some validations for movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 Movie.destroy_all
 
 movies = Movie.create([
-  {title: 'The Lego Movie', total_gross: 257756197, release_date: DateTime.new(2014,2,07,9), mpaa_rating: 'pg', description: 'rate this movie!'},
-  {title: '22 Jump Street', total_gross: 191616238, release_date: DateTime.new(2014,6,13,9), mpaa_rating: 'r', description: 'rate this movie!'},
-  {title: 'Gone Girl', total_gross: 167735396, release_date: DateTime.new(2014,10,03,9), mpaa_rating: 'r', description: 'rate this movie!'}
+  {title: 'The Lego Movie', total_gross: 257756197, release_date: DateTime.new(2014,2,07,9), mpaa_rating: 'PG', description: 'rate this movie!'},
+  {title: '22 Jump Street', total_gross: 191616238, release_date: DateTime.new(2014,6,13,9), mpaa_rating: 'R', description: 'rate this movie!'},
+  {title: 'Gone Girl', total_gross: 167735396, release_date: DateTime.new(2014,10,03,9), mpaa_rating: 'R', description: 'rate this movie!'}
   ])
 
 movies[0].reviews << Review.create([
