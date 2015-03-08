b7af0bf749c3701d52b86f82b661f66b599f52ec
Helen Thi <ht5658@gmail.com>
2015-03-08 12:21:20 -0400

    added review routes for update and destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :movies, only: [:index, :create, :show, :update, :destroy] do
-    resources :reviews, only: [:index, :create]
+    resources :reviews, only: [:index, :create, :update, :destroy]
   end
 end
