b28cab5bb80d1a315b48c1e719291275fa195794
ironman <ironman@Brandons-MacBook-Pro.local>
2015-03-09 15:43:01 -0400

    updated routes with correct "end" at the end of file

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :movies, only: [:index, :create, :show, :update, :destroy] do
     resources :reviews, only: [:index, :create, :update, :destroy]
   end
-end
+
 
 
   namespace :api do
@@ -13,7 +13,7 @@ end
       end
     end
 end
-
+end
 
 
 
