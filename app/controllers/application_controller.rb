a0dd905879f44416d3013d714f1bcf2b2f144e62
Helen Thi <ht5658@gmail.com>
2015-03-09 15:29:39 -0400

    added user authenticate method to the applicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::API
   include ActionController::Serialization
+
+  protected
+  def authenticate
+    authenticate_or_request_with_http_token do |token, options|
+      User.find_by(token: token)
+  end
 end
