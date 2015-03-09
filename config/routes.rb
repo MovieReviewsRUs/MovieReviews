45160e74e98866eac036b7af8605861afc0e513e
ironman <ironman@Brandons-MacBook-Pro.local>
2015-03-09 15:33:19 -0400

    updated routes.rb for login auth.

--- config/routes.rb
+++ config/routes.rb
@@ -3,3 +3,29 @@ Rails.application.routes.draw do
     resources :reviews, only: [:index, :create, :update, :destroy]
   end
 end
+
+
+  namespace :api do
+    namespace :v1 do
+      resources :posts, only: [:index]
+      resources :users, defaults: { format: :json }, only: [:show, :index, :create] do
+        post 'sign_in', on: :collection
+      end
+    end
+end
+
+
+
+
+
+
+# Rails.application.routes.draw do
+#   namespace :api do
+#     namespace :v1 do
+#       resources :posts, only: [:index]
+#       resources :users, defaults: { format: :json }, only: [:show, :index, :create] do
+#         post 'sign_in', on: :collection
+#       end
+#     end
+#   end
+# end
