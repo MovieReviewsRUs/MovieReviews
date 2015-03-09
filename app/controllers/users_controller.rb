7aca21dc50b06c2adc8d6f359354d54893e1783b
pat <patrick.h.needham@gmail.com>
2015-03-09 15:34:17 -0400

    completed users controller with sign_in, index, show, and create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,40 @@
-class UsersController < ApplicationController
+module Api
+  module V1
+    class UsersController < ApplicationController
 
-  before_filter :authenticate, only: [:index]
+      before_filter :authenticate, only: [:index]
 
+      def sign_in
+        user = User.find_by(email: params[:email])
+        if user && user.authenticate(params[:password])
+          render json: { token: user.token }
+        else
+          head :unauthorized
+        end
+      end
 
+      def index
+        render json: User.all, status: :ok
+      end
 
+      def show
+        @user = User.find(params[:id])
+        render json: @user
+      end
 
+      def create
+        @user = User.new(user_params)
+        if @user.save
+          render json: {token: @user.token}
+        else
+          render json: {message: 'failed', status: 500}
+        end
+      end
+
+      def user_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation, :token)
+      end
+
+    end
+  end
 end
